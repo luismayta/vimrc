ddb0fa555e7a4069911e2e6b3113336f13307632
@slovacus <slovacus@gmail.com>
2020-02-16 10:11:57 -0500

    feat: load value for docker (#12)

--- provision/make/tests.mk
+++ provision/make/tests.mk
@@ -4,12 +4,15 @@ test.help:
 	@echo '        test                      show help'
 	@echo '        test.all   	             Run all module test'
 	@echo '        test.lint                 Run all pre-commit'
+	@echo '        test.syntax             Run all syntax in code'
 	@echo ''
 
 test:
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
 	@if [ -z "${run}" ]; then \
-		make test.help;\
+		$(docker-test-run) bash -c "$(PIPENV_RUN) pytest"; \
+	elif [ -n "${run}" ]; then \
+		$(docker-test-run) bash -c "$(PIPENV_RUN) pytest tests/${run}"; \
 	fi
 
 test.all:
@@ -18,3 +21,6 @@ test.all:
 
 test.lint:
 	$(PIPENV_RUN) pre-commit run --all-files --verbose
+
+test.syntax:
+	@echo $(MESSAGE) Running tests $(END)
